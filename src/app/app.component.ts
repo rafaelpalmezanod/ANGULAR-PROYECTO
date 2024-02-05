73e4bf679c0ad79d939b53cea2904d0ddbc4d442
rafaelpalmezanod <rafaelandrespalmezanodias@gmail.com>
2024-02-05 17:13:05 -0500

    cmabios2

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -2,15 +2,19 @@ import { Component } from '@angular/core';
 import { RouterOutlet } from '@angular/router';
 import { HeaderComponent } from './header/header.component';
 import { FooterComponent } from './footer/footer.component';
-import { MainComponent} from './main/main.component'
+import { MainComponent} from './main/main.component';
+import {AboutComponent} from './about/about.component'
+
+
+
 
 @Component({
   selector: 'app-root',
   standalone: true,
-  imports: [RouterOutlet, HeaderComponent ,FooterComponent, MainComponent],
+  imports: [RouterOutlet, HeaderComponent ,FooterComponent, MainComponent, AboutComponent ],
   templateUrl: './app.component.html',
   styleUrl: './app.component.css'
 })
 export class AppComponent {
   title = 'Primer-Curso';
-}
+} 
