6b06b1112209684975e24131be8febd4bf8c9316
Rafael Andres Palmezano Diaz <rafaelandrespalmezanodias@gmail.com>
2024-02-06 21:59:54 -0500

    cambios

--- src/app/about/about.component.ts
+++ src/app/about/about.component.ts
@@ -1,9 +1,10 @@
 import { Component } from '@angular/core';
+import { RouterLink, RouterOutlet } from '@angular/router';
 
 @Component({
   selector: 'app-about',
   standalone: true,
-  imports: [],
+  imports: [RouterOutlet , RouterLink],
   templateUrl: './about.component.html',
   styleUrl: './about.component.css'
 })
--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -1,9 +1,11 @@
 import { Routes } from '@angular/router';
 import { AboutComponent } from './about/about.component';
 import { ContactComponent } from './contact/contact.component';
+import {MainComponent} from './main/main.component'
 
 export const routes: Routes = [
     { path:'about' , component: AboutComponent},
     { path:'contact' , component: ContactComponent},
+    { path:'home', component: MainComponent},
    
 ];
