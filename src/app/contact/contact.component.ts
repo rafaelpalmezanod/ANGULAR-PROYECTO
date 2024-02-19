1e88674dc7da92f93372a6cf9c0b98246b37e0dd
Rafael Andres Palmezano Diaz <rafaelandrespalmezanodias@gmail.com>
2024-02-18 22:00:42 -0500

    cambios

--- src/app/contact/contact.component.ts
+++ src/app/contact/contact.component.ts
@@ -1,14 +1,33 @@
 import { Component } from '@angular/core';
+import { FormControl, FormGroup, ReactiveFormsModule, Validators } from '@angular/forms';
 import { RouterLink } from '@angular/router';
 
 @Component({
   selector: 'app-contact',
   standalone: true,
-  imports: [RouterLink],
+  imports: [RouterLink, ReactiveFormsModule],
   templateUrl: './contact.component.html',
   styleUrl: './contact.component.css',
   
 })
 export class ContactComponent {
+  Formulario = new FormGroup({
+    nombre: new FormControl('',Validators.required),
+    apellido: new FormControl('',Validators.required),
+    email: new FormControl('',[Validators.required, Validators.email]),
+    telefono: new FormControl('',Validators.required),
+  });
+
+  constructor(){}
+
+  onSubmit() {
+    if (this.Formulario.valid) {
+      console.log('Form Submitted:', this.Formulario.value);
+    } else {
+      this.Formulario.markAllAsTouched();
+    }
+  }
+
+  
 
 }
